751de76c9578f04008b703ba8843f6ec8dbc3ab7
Antoine Trouvé <trouve.antoine@icloud.com>
2023-09-15 06:58:26 +0900

    Improve parseTextBlock

--- src/parse-text-block.ts
+++ src/parse-text-block.ts
@@ -1,11 +1,20 @@
+const BUILTIN_COMMANDS = {
+    "year": args => {
+        return (new Date()).getFullYear().toString();
+    }
+}
+
 export function parseTextBlock(
     txt: string,
-    commands: { [cmd: string]: (args: string[]) => string } = {
-        "year": args => {
-            return (new Date()).getFullYear().toString();
-        }
-    }
+    commands: { [cmd: string]: (args: string[]) => string } = {},
+    tagWhiteList: string[] = [
+        "kbd", "abbr", "b", "bdi", "bdo", "br", "code", "data", "time", "dfn", "em", "ti", "mark",
+        "q", "rp", "rt", "rtc", "ruby", "s", "samp", "small", "span", "strong", "sub", "sup",
+        "u", "var", "wbr", "del", "ins"
+    ]
 ) {
+    commands = { ...BUILTIN_COMMANDS, ...commands };
+
     /** Replace the [!cmd arg arg] */
     let transformed = txt.replaceAll(/\[\!(\w[\w-\s]+)\]/g, s => {
         const ss = s.slice(2, -1).split(/\s+/);
@@ -23,13 +32,17 @@ export function parseTextBlock(
     })
 
     /** Replace the [html-element content] */
-    transformed = transformed.replaceAll(/\[(\w[\w-\s]+)\]/g, s => {
+    transformed = transformed.replaceAll(/[^!]\[(\w[\w-\s]+)\][^(]/g, s => {
         const ss = s.slice(1, -1).split(/\s+/, 1);
         if (ss.length === 0) {
             console.warn("Empty HTML command", s)
             return s;
         }
         const tagName = ss[0];
+        if (!tagWhiteList.includes(tagName)) {
+            console.warn("Non-whitelisted HTML tag", tagName, "in", s);
+            return s;
+        }
         const content = s.slice(2 + tagName.length + 1, -1);
 
         return `<${tagName}>${content}</${tagName}>`
